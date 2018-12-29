81b2aa5496dba3f20bc61b723b8fb7327c0f7979
Dave Longbottom <davelongbottom@gmail.com>
2018-12-30 08:43:29 +1030

    async fetch working using thunk dispatch

--- 4-redux/src/js/client.js
+++ 4-redux/src/js/client.js
@@ -1,12 +1,58 @@
 import { applyMiddleware, createStore } from "redux";
+import axios from "axios"
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 
-const reducer = function(state={}, action) {
+const initialState = {
+  fetching: false,
+  fetched: false,
+  users: [],
+  error: null,
+}
 
+const reducer = function(state=initialState, action) {
+
+  switch(action.type) {
+    case "FETCH_USERS_REQUEST" : {
+      return {
+        ...state, 
+        fetching: true
+      }
+      break
+    }
+    case "FETCH_USERS_RESPONSE" : {
+      return {
+        ...state, 
+        fetching: false, 
+        fetched: true, 
+        users: action.payload
+      }
+      break
+    }
+    case "FETCH_USERS_ERROR" : {
+      return {...state, 
+        fetching: false, 
+        error: action.payload
+      }
+      break
+    }
+  }
   return state;
 }
 
-const middleware = applyMiddleware()
+const middleware = applyMiddleware(thunk, logger())
 
-const store = createStore(reducer, 0, middleware);
+const store = createStore(reducer, middleware);
 
-store.dispatch({type: "INC", payload: 1});
+store.dispatch(dispatch => {
+  dispatch({type: "FETCH_USERS_REQUEST"})
+  //axios.get('http://rest.learncode.academy/api/wstern/users')
+  axios.get('https://randomuser.me/api/?results=2')
+  .then(response => {
+      //do something asnyc
+      dispatch({type: "FETCH_USERS_RESPONSE", payload: response.data.results})
+  })
+  .catch(err => {
+    dispatch({type: "FETCH_USERS_ERROR", payload: err})
+  })
+});

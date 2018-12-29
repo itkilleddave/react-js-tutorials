bfed5549593a1eb25be9f62806a3ffb410f2ddbb
Dave Longbottom <davelongbottom@gmail.com>
2018-12-29 17:28:54 +1030

    basic store updates

--- 4-redux/src/js/client.js
+++ 4-redux/src/js/client.js
@@ -1,1 +1,29 @@
 import { createStore } from "redux";
+
+const reducer = function(state, action) {
+  
+  if (action.type === "INC") {
+    return state+action.payload
+  } 
+  if (action.type === "DEC") {
+    return state-action.payload
+  } 
+  return state;
+}
+
+const store = createStore(reducer, 0);
+
+store.subscribe(() => {
+  console.log("store changed", store.getState())
+})
+
+//INC
+store.dispatch({type: "INC", payload: 1});
+store.dispatch({type: "INC", payload: 1});
+store.dispatch({type: "INC", payload: 1});
+store.dispatch({type: "INC", payload: 1});
+store.dispatch({type: "INC", payload: 10});
+//DEC
+store.dispatch({type: "DEC", payload: 1});
+store.dispatch({type: "DEC", payload: 1});
+store.dispatch({type: "DEC", payload: 1});

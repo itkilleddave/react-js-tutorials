fb07053da37644ead3d6a80f7dfdba7fda4114af
Dave Longbottom <davelongbottom@gmail.com>
2018-12-29 18:57:05 +1030

    completed tutorial 5 (adding middleware)

--- 4-redux/src/js/client.js
+++ 4-redux/src/js/client.js
@@ -1,4 +1,5 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+
 
 const reducer = function(state, action) {
   
@@ -8,11 +9,33 @@ const reducer = function(state, action) {
   if (action.type === "DEC") {
     return state-action.payload
   } 
+  if (action.type == "E") {
+    throw new Error("Whoops!")
+  }
   return state;
 }
 
-const store = createStore(reducer, 0);
+const logger = (store) => (next) => (action) => {
+  console.log("action fired", action)
+  // action.payload = 8 // e.g. can override params
+  next(action)
+}
 
+const error = (store) => (next) => (action) => {
+  try {
+    next(action)
+  } catch (e) {
+    console.log("AHHHH!", e)
+  }
+}
+
+const middleware = applyMiddleware(logger, error)
+
+const store = createStore(reducer, 0, middleware);
+
+store.subscribe(() => {
+  console.log('store changed', store.getState());
+})
 
 //INC
 store.dispatch({type: "INC", payload: 1});
@@ -23,4 +46,4 @@ store.dispatch({type: "INC", payload: 10});
 //DEC
 store.dispatch({type: "DEC", payload: 1});
 store.dispatch({type: "DEC", payload: 1});
-store.dispatch({type: "DEC", payload: 1});
+store.dispatch({type: "E", payload: 1});
